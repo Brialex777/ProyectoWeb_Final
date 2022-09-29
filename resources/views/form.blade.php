1e936da660807a622a190ac31292d350848d2945
Brian Magana <brian.magana2929@alumnos.udg.mx>
2022-09-28 20:19:05 -0500

    Commit Inicial

---
+++ resources/views/form.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    <form action="producto" method="post">
+        <label for="nombre">Nombre:</label><br>
+        <input type="text" name="nombre" id="name">
+
+        <label for="precio">Precio:</label><br>
+        <input type="text" name="precio" id="cash">
+
+        <label for="Cantidad">Cantidad:</label><br>
+        <input type="text" name="cantidad" id="quantity">
+
+        <input type="submit" value="Crear">
+    </form>
+</body>
+</html>
